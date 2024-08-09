22a9d7b94665d4cdde9e8303337847b9bac5b302
tividan2014 <tividan@gmail.com>
2024-08-09 17:32:12 +0300

    fix useRandomColor

--- src/common/hooks/useRandomColor.ts
+++ src/common/hooks/useRandomColor.ts
@@ -1,21 +1,16 @@
 import { Color } from 'features/image/store/types'
-import { useState, useEffect } from 'react'
 
 const getRandom = (maxValue: number) => {
   return Math.floor(Math.random() * maxValue)
 }
 
 const useRandomColor = (ignoreAlpha: boolean = true): Color => {
-  const [color, setColor] = useState<Color>({ r: 0, g: 0, b: 0, a: 1 })
-
-  useEffect(() => {
-    setColor({
-      r: getRandom(255),
-      g: getRandom(255),
-      b: getRandom(255),
-      a: ignoreAlpha ? 1 : getRandom(100),
-    })
-  }, [ignoreAlpha])
+  const color: Color = {
+    r: getRandom(255),
+    g: getRandom(255),
+    b: getRandom(255),
+    a: ignoreAlpha ? 1 : getRandom(100),
+  }
 
   return color
 }
